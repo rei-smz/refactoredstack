f933fdb1730e12ed7ddd30adf5d847aad4ddaa06
Rin Shimizu <52402142+rei-smz@users.noreply.github.com>
2024-03-06 19:54:42 -0500

    Removed extends

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -3,19 +3,19 @@ package org.example;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 
-public class MyStack<E> extends ArrayList<E> {
+public class MyStack<E> {
     private ArrayList<E> delegate;
     public MyStack() {
-        delegate = this;
+        delegate = new ArrayList<>();
     }
     public void push(E e) {
-        this.add(e);
+        delegate.add(e);
     }
 
     public E pop() {
-        if (this.isEmpty()) throw new EmptyStackException();
-        E e = this.get(this.size() - 1);
-        this.remove(this.size() -1 );
+        if (delegate.isEmpty()) throw new EmptyStackException();
+        E e = delegate.get(delegate.size() - 1);
+        delegate.remove(delegate.size() -1 );
         return e;
     }
 
