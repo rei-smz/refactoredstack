e18193db22b50181962e3f1408207f9d0a7ffb52
Rin Shimizu <52402142+rei-smz@users.noreply.github.com>
2024-03-06 19:49:20 -0500

    Created delegate

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.EmptyStackException;
 
 public class MyStack<E> extends ArrayList<E> {
-
+    private ArrayList<E> delegate;
+    public MyStack() {
+        delegate = this;
+    }
     public void push(E e) {
         add(e);
     }
